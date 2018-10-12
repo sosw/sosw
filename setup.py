283dd71d27155ff31fe0188431a9580af4d50860
ngr <grischenko.n@gmail.com>
2018-10-12 15:35:59 -0700

    init package

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 
 setup(name='sosww',
-      version='0.1',
-      description='Serverless Orchestrator of Serverless Workers - Worker client',
+      version='0.1.3',
+      description='SOSW Worker',
       url='http://github.com/bimpression/sosww',
       author='Nikolay Grishchenko',
       author_email='nikolay@bimpression.com',
       license='MIT',
       classifiers=[
+          'Development Status :: 1 - Planning',
           'Operating System :: Other OS',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.6',
@@ -16,6 +17,6 @@ setup(name='sosww',
       ],
       packages=['sosww'],
       install_requires=[
-          'boto3',
+          'boto3>=1.9'
       ],
       zip_safe=False)
