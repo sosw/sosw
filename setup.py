fea619e168222a649f6d4512d6c9930a5bddf036
Nikolay Grishchenko <grischenko.n@gmail.com>
2019-01-28 15:54:13 +0200

    v0.3.9 components.dynamo_db.batch_get_items

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='sosw',
-      version='0.3.7',
+      version='0.3.9',
       description='Serverless Orchestrator of Serverless Workers',
       url='http://github.com/bimpression/sosw',
       author='Nikolay Grishchenko',
