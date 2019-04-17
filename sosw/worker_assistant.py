1a625a6984af8471533b70a10ee298dd1cb8db61
sophie <sophie.fogel@gmail.com>
2019-04-17 12:20:20 +0300

    worker_assistant call + test

--- sosw/worker_assistant.py
+++ sosw/worker_assistant.py
@@ -2,6 +2,7 @@ import time
 
 from sosw import Processor
 from sosw.components.dynamo_db import DynamoDbClient
+from sosw.components.helpers import get_one_from_dict
 
 
 class WorkerAssistant(Processor):
@@ -32,7 +33,33 @@ class WorkerAssistant(Processor):
     dynamo_db_client: DynamoDbClient = None
 
 
+    def __call__(self, event):
+        action = get_one_from_dict(event, 'action', str)
+
+        mapper = {
+            'mark_task_as_completed': {
+                'function': self.mark_task_as_completed,
+                'required_params': ['task_id']
+            }
+        }
+
+        if action in mapper:
+            func = mapper[action]['function']
+            required_params = mapper[action]['required_params']
+
+            for rp in required_params:
+                if rp not in event:
+                    raise Exception(f"Missing required parameter `{rp}` in event for action `{action}`")
+
+            func_kwargs = {k: event[k] for k in event if k in required_params}
+            return func(**func_kwargs)
+        else:
+            raise Exception(f"Action `{action}` is not supported")
+
+
     def mark_task_as_completed(self, task_id: str):
+        assert isinstance(task_id, str), f"`task_id` must be a string"
+
         _ = self.get_db_field_name
 
         self.dynamo_db_client.update(
