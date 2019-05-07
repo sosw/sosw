8f42c66ed6906ca8257d94f60f104291a15bfd16
drugbug <gil_halperin@hotmail.com>
2019-05-07 19:31:27 +0300

    return lambda function timeout

--- sosw/managers/ecology.py
+++ sosw/managers/ecology.py
@@ -32,6 +32,7 @@ ECO_STATUSES = (
 
 class EcologyManager(Processor):
     DEFAULT_CONFIG = {
+        'init_clients': ['lambda']
     }
 
     running_tasks = defaultdict(int)
@@ -125,10 +126,10 @@ class EcologyManager(Processor):
         """
         Maximum duration of `labourer` executions.
         Should ask this from aws:lambda API, but at the moment use the hardcoded maximum.
-        # TODO implement me.
         """
 
-        return 900
+        resp = self.lambda_client.get_function_configuration(FunctionName=labourer.arn)
+        return resp['Timeout']
 
 
     # The task_client of EcologyManager is just a pointer. We skip recursive stats to avoid infinite loop.
