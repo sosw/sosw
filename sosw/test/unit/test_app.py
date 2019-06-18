1a3f6adb20a82a2d1b032e56bd5d06d3b597ad9a
nimirium <sophie.fogel@gmail.com>
2019-06-18 14:58:39 +0300

    tests

--- sosw/test/unit/test_app.py
+++ sosw/test/unit/test_app.py
@@ -3,9 +3,9 @@ import os
 import unittest
 
 from unittest import mock
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
-from sosw.app import Processor, LambdaGlobals, get_lambda_handler
+from sosw.app import Processor, LambdaGlobals, get_lambda_handler, logger
 from sosw.components.sns import SnsManager
 from sosw.components.siblings import SiblingsManager
 
@@ -18,11 +18,24 @@ class app_UnitTestCase(unittest.TestCase):
     TEST_CONFIG = {'test': True}
 
 
+    class Child(Processor):
+        def __call__(self, event):
+            super().__call__(event)
+            return event.get('k')
+
+
     def setUp(self):
-        pass
+        self.get_config_patch = mock.patch("sosw.app.get_config")
+        self.get_config_mock = self.get_config_patch.start()
+
+        self.set_level_patch = patch.object(logger, 'setLevel')
+        self.set_level_mock = self.set_level_patch.start()
 
 
     def tearDown(self):
+        self.get_config_patch.stop()
+        self.set_level_patch.stop()
+
         try:
             del (os.environ['AWS_LAMBDA_FUNCTION_NAME'])
         except:
@@ -82,14 +95,13 @@ class app_UnitTestCase(unittest.TestCase):
         mock_boto_client.assert_called_with('not_exists')
 
 
-    @mock.patch("sosw.app.get_config")
-    def test_app_calls_get_config(self, mock_ssm):
+    def test_app_calls_get_config(self):
 
-        mock_ssm.return_value = {'mock': 'called'}
+        self.get_config_mock.return_value = {'mock': 'called'}
         os.environ['AWS_LAMBDA_FUNCTION_NAME'] = 'test_func'
 
         Processor(custom_config=self.TEST_CONFIG)
-        mock_ssm.assert_called_once_with('test_func_config')
+        self.get_config_mock.assert_called_once_with('test_func_config')
 
 
     # @unittest.skip("https://github.com/bimpression/sosw/issues/40")
@@ -104,22 +116,25 @@ class app_UnitTestCase(unittest.TestCase):
 
     def test_lambda_handler(self):
 
-        class Child(Processor):
-            def __call__(self, event):
-                super().__call__(event)
-                return event.get('k')
-
         global_vars = LambdaGlobals()
         self.assertIsNone(global_vars.processor)
         self.assertIsNone(global_vars.lambda_context)
 
-        lambda_handler = get_lambda_handler(Child, global_vars, self.TEST_CONFIG)
+        lambda_handler = get_lambda_handler(self.Child, global_vars, self.TEST_CONFIG)
         self.assertIsNotNone(lambda_handler)
 
         for i in range(3):
             result = lambda_handler(event={'k': 'success'}, context={'context': 'test'})
-            self.assertEqual(type(global_vars.processor), Child)
+            self.assertEqual(type(global_vars.processor), self.Child)
             self.assertEqual(global_vars.lambda_context, {'context': 'test'})
             self.assertEqual(result, 'success')
             self.assertEqual(global_vars.processor.stats['total_processor_calls'], i + 1)
             self.assertEqual(global_vars.processor.stats['total_calls_register_clients'], 1)
+
+
+    def test_lambda_handler__logger_level(self):
+        global_vars = LambdaGlobals()
+        lambda_handler = get_lambda_handler(self.Child, global_vars, self.TEST_CONFIG)
+        event = {'k': 'm', 'logging_level': 20}
+        lambda_handler(event=event, context={'context': 'test'})
+        self.set_level_mock.assert_called_once_with(20)
