6f465d87eb3491070a7f9203c775e06648bc734b
ngr <nikolay@bimpression.com>
2019-08-11 19:51:44 +0300

    v0.7.23

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='sosw',
-      version='0.7.21',
+      version='0.7.23',
       description='Serverless Orchestrator of Serverless Workers',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='http://github.com/sosw/sosw',
       author='Nikolay Grishchenko',
-      author_email='nikolay@bimpression.com',
-      license='The MIT License (MIT)',
+      author_email='ngr@sosw.app',
+      license='MIT',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Operating System :: Other OS',
@@ -21,7 +21,6 @@ setup(name='sosw',
           'Programming Language :: Python :: 3.7',
           'Topic :: Software Development'
       ],
-      # packages=['sosw'],
       packages=find_packages(exclude=['docs', 'test', 'examples']),
       install_requires=[
           'boto3>=1.9'
