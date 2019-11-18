9ce594d2d42bbdc6dfd2abcc53e772b0512873fb
ngr <nikolay@bimpression.com>
2019-11-18 12:29:31 +0200

    init version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='sosw',
-      version='0.7.25',
+      version='0.7.27',
       description='Serverless Orchestrator of Serverless Workers',
       long_description=long_description,
       long_description_content_type="text/markdown",
