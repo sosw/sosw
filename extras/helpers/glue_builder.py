686c5b6b3dc2bc7154a5a4516efcbf4fc4695eba
Julia Rozova <julia.in.your.heart@gmail.com>
2024-06-25 13:22:40 +0300

    updated glue helper

--- extras/helpers/glue_builder.py
+++ extras/helpers/glue_builder.py
@@ -8,32 +8,33 @@ logging.basicConfig(level=logging.DEBUG,
                         logging.FileHandler("glue_operations.log"),
                         logging.StreamHandler()
                     ])
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 logging.getLogger('botocore').setLevel(logging.WARNING)
 logging.getLogger('boto3').setLevel(logging.WARNING)
 
-glue_client = boto3.client('glue')
+session = boto3.Session(region_name='us-east-1')
+glue_client = session.client('glue')
 
+def list_databases() -> list:
+    databases =[]
 
-def list_of_tables():
-    tables = []
-
-    logger.info("Starting to get databases from AWS Glue")
     paginator = glue_client.get_paginator('get_databases')
     for page in paginator.paginate():
         for database in page['DatabaseList']:
-            database_name = database['Name']
-            logger.debug(f"Found database: {database_name}")
+            databases.append(database['Name'])
+            logger.debug(f"Found database: {database['Name']}")
 
-            table_paginator = glue_client.get_paginator('get_tables')
-            for table_page in table_paginator.paginate(DatabaseName=database_name):
-                for table in table_page['TableList']:
-                    table_name = table['Name']
-                    tables.append({
-                        'Database': database_name,
-                        'Table': table_name
-                    })
-                    logger.debug(f"Found table: {table_name} in database: {database_name}")
+    return databases
+
+
+def list_tables(database: str) -> list:
+    tables = []
+
+    table_paginator = glue_client.get_paginator('get_tables')
+    for table_page in table_paginator.paginate(DatabaseName=database):
+        for table in table_page['TableList']:
+            tables.append(table['Name'])
+            logger.debug("Found table: %s in database: %s", table['Name'], database)
 
     logger.info("Completed getting all tables from AWS Glue")
     return tables
@@ -41,10 +42,6 @@ def list_of_tables():
 
 if __name__ == '__main__':
     logger.info("Application started")
-    try:
-        all_tables = list_of_tables()
-        for table in all_tables:
-            print(f"Database: {table['Database']}, Table: {table['Table']}")
-            logger.info(f"Database: {table['Database']}, Table: {table['Table']}")
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
+    if dbs := list_databases():
+        for db in dbs:
+            tables = list_tables(db)
