db2ec136605152f9e2ad17fa3cf5643f32868266
Julia Rozova <julia.in.your.heart@gmail.com>
2024-06-25 15:19:53 +0300

    updated glue builder

--- extras/helpers/glue_builder.py
+++ extras/helpers/glue_builder.py
@@ -17,11 +17,13 @@ logging.getLogger('boto3').setLevel(logging.WARNING)
 
 class GlueBuilder(SoswProcessor):
     DEFAULT_CONFIG = {
-        'init_clients': ['glue', 'dynamodb'],
+        'init_clients': ['glue', 'dynamodb', 'iam'],
     }
 
-    glue_client: boto3.client = None
     dynamodb_client: boto3.client = None
+    glue_client: boto3.client = None
+    iam_client: boto3.client = None
+
 
 
     def list_glue_databases(self) -> list:
@@ -48,8 +50,62 @@ class GlueBuilder(SoswProcessor):
         logger.info("Completed getting all tables from AWS Glue")
         return tables
 
+
+    def create_role_for_crawler(self, name: str) -> str:
+        arn = ''
+
+        self.iam_client.create_role()
+
+        self.iam_client.attach_role_policy()
+
+        self.iam_client.put_role_policy()
+
+        return arn
+
+
+    def create_crawler_for_ddb(self, tablename: str) -> str:
+        crawler_name = f'ddb_{tablename}_crawler'
+        if crawler_name in self.get_crawlers():
+            logger.info("%s crawler already exists", crawler_name)
+            return crawler_name
+
+        self.glue_client.create_crawler(
+            Name=crawler_name,
+            Role=self.create_role_for_crawler(name=crawler_name),
+            DatabaseName='ddb_tables',
+            Targets={
+                'DynamoDBTargets': [
+                    {
+                        'Path': tablename,
+                    },
+                ],
+            }
+        )
+        logger.info("Created crawler %s", crawler_name)
+        return crawler_name
+
+
+    def get_crawlers(self) -> list:
+        result = []
+        response = self.glue_client.list_crawlers()
+        if 'CrawlerNames' in response:
+            result.extend(response['CrawlerNames'])
+
+        while pagination_token := response.get('NextToken'):
+            logger.debug("Paginating from token: %s", pagination_token)
+            response = self.glue_client.list_crawlers(NextToken=pagination_token)
+            if 'CrawlerNames' in response:
+                result.extend(response['CrawlerNames'])
+        logger.info("Found crawlers: %s", result)
+        return result
+
+
+
     def create_crawlers_for_ddbs(self):
-        pass
+        tables = self.get_ddb_tables()
+        for table in tables:
+
+            self.create_crawler_for_ddb(table)
 
 
     def get_ddb_tables(self) -> list:
@@ -75,3 +131,5 @@ if __name__ == '__main__':
     if dbs := glue_builder.list_glue_databases():
         for db in dbs:
             tables = glue_builder.list_glue_tables(db)
+
+    glue_builder.create_crawlers_for_ddbs()
