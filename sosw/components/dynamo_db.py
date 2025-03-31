354cb4c5b819acef59dea7bbce997db54d204791
kreanimator <backin.v@yandex.ru>
2025-03-31 11:47:51 +0300

    Fix #361

--- sosw/components/dynamo_db.py
+++ sosw/components/dynamo_db.py
@@ -680,7 +680,7 @@ class DynamoDbClient:
 
 
     def _parse_filter_expression(self, expression: str) -> Tuple[str, Dict]:
-        """
+        r"""
         Converts FilterExpression to Dynamo syntax. We still do not support some operators. Feel free to implement:
         `Expressions.OperatorsAndFunctions.html
         <https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.OperatorsAndFunctions.html>`_
